2284738a3f2a29f2c2f5ae9882c351d1e2e69e69
Sean <sean_obeirne@outlook.com>
2023-04-19 10:23:29 +0100

    Added get request that links to local host 3000

--- server/app.js
+++ server/app.js
@@ -4,6 +4,10 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
+const facts = require("./facts")
 
+app.get("/", (req, res) => {
+    res.send(`Welcome to the facts API! There are ${facts.length} available.`);
+  });
 
 module.exports = app
