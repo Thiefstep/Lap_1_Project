edefa29bed8f99fa96c7b7f8862fa0929c9cfd83
Leahjthom <leahjthom@gmail.com>
2023-04-19 12:19:04 +0100

    Updated the endpoit to show the list of facts in the API

--- server/app.js
+++ server/app.js
@@ -1,1 +1,22 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+app.use(cors());
+
+let facts = require("./facts") 
+
+app.get("/", (req, res) => {
+    res.send(`Welcome to the facts API! There are ${facts.length} available.`);
+  });
+
+  app.get("/facts", (req, res) => {
+    facts = facts.map((fact, i) => {
+      fact["id"] = i + 1;
+      return fact;
+    });
+    res.json(facts);
+  });
+
+
+module.exports = app
--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const app = require('./appfacts');
+const app = require("./app");
 const port = 3000
 
 app.listen(port, () => {
