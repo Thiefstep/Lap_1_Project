350c5e88e36f39365dc3c74e480ce95f99464311
Leahjthom <leahjthom@gmail.com>
2023-04-19 11:54:45 +0100

    created an appfacts.js

---
+++ appfacts.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const facts = require("./facts") 
+
+app.get("/", (req, res) => {
+    res.send(`Welcome to the facts API! There are ${facts.length} available.`);
+  });
+
+
+module.exports = appfacts
--- server/app.js
+++ server/app.js
@@ -1,14 +1,1 @@
-const express = require("express");
-const cors = require("cors");
 
-const app = express();
-app.use(cors());
-
-const facts = require("./facts") 
-
-app.get("/", (req, res) => {
-    res.send(`Welcome to the facts API! There are ${facts.length} available.`);
-  });
-
-
-module.exports = app
