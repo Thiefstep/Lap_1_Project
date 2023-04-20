57a532d1814e519d299ab7749d544e78c8c70c86
Chey-DB <cheydb@rocketmail.com>
2023-04-20 15:38:58 +0100

    accessing elements in the HTML and storing them in variables

---
+++ client/game.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+const questionEl = document.getElementById("question");
+const yellowEl = document.getElementById("yellow");
+const redEl = document.getElementById("red");
+const scoreEl = document.getElementById("score");
+const highscoreEl = document.getElementById("highscore");
