9348ea99c4c9a7fe16b3fa05c920945e8f9aac9d
Chey-DB <cheydb@rocketmail.com>
2023-04-20 15:40:13 +0100

    Created all game variables that will be updated

--- client/game.js
+++ client/game.js
@@ -5,3 +5,12 @@ const yellowEl = document.getElementById("yellow");
 const redEl = document.getElementById("red");
 const scoreEl = document.getElementById("score");
 const highscoreEl = document.getElementById("highscore");
+
+// Game variables
+let snake;
+let food;
+let score;
+// let highscore;
+let highscore = 0;
+let direction;
+let gameInterval;
