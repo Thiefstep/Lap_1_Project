80318a37c818b6da299f3e61a4ed790befcd3410
Thiefstep <matzz1@hotmail.co.uk>
2023-04-20 16:14:52 +0100

    More Bug fixes

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const questions = require('./questions.json')
+//const questions = require('./questions.json')
 const logger = require("./logger");
 
 const app = express();
@@ -41,27 +41,27 @@ app.get("/", (req, res) => {
     }
   });
 
-app.get('/', (req, res) => { 
-    res.send(`Welcome to the questions API! There are ${questions} available.`);
-})
+// app.get('/', (req, res) => { 
+//     res.send(`Welcome to the questions API! There are ${questions} available.`);
+// })
 
-app.get('/questions', (req, res) => { 
-    res.send(questions);
-})
+// app.get('/questions', (req, res) => { 
+//     res.send(questions);
+// })
 
-app.get('/questions/random', (req, res) => { 
-    const randIdx = Math.floor(Math.random()* questions.length);
-    res.send(questions[randIdx]);
-})
+// app.get('/questions/random', (req, res) => { 
+//     const randIdx = Math.floor(Math.random()* questions.length);
+//     res.send(questions[randIdx]);
+// })
 
-app.get('/questions/:id', (req, res) => { 
-    const idx = req.params.id;
-    const questions = questions.find(q => q.id === idx);
-    if(!questions){
-        res.status(404).send({error: "Quote not found"});
-    }else
-    res.send(questions); 
-})
+// app.get('/questions/:id', (req, res) => { 
+//     const idx = req.params.id;
+//     const questions = questions.find(q => q.id === idx);
+//     if(!questions){
+//         res.status(404).send({error: "Quote not found"});
+//     }else
+//     res.send(questions); 
+// })
 
 module.exports = app;
 
